d2a89cc262aabbce47eca0f7d117dcfed826e4f9
amen souissi <amensouissi@ecreall.com>
2014-02-19 17:16:25 +0100

    savepoint

---
+++ dace/catalog/object.py
@@ -0,0 +1,42 @@
+from zope.interface import Interface, Declaration, implements, providedBy
+from pyramid.threadlocal import get_current_registry
+from zope.location.interfaces import ILocation
+
+from substanced.catalog import (
+    catalog_factory,
+    Keyword,
+    )
+
+class IObjectProvides(Interface):
+    def object_provides():
+        pass
+
+
+@indexview_defaults(catalog_name='objectprovidesindexes')
+class ObjectProvidesViews(object):
+    def __init__(self, resource):
+        self.resource = resource
+
+    @indexview()
+    def object_provides(self, default):
+        adapter = get_current_registry().queryAdapter(self.resource,IObjectProvides)
+        if adapter is None:
+            return default
+        return adapter.process_id()
+
+
+@catalog_factory('objectprovidesindexes')
+class ObjectProvidesIndexes(object):
+    #grok.context(IObjectProvides)
+
+    object_provides = Keyword()
+
+
+@adapter(context = ILocation, name = u'objectprovides' )
+class ObjectProvides(Adapter):
+    """Return provided interfaces of the object.
+    """
+    implements(IObjectProvides)
+
+    def object_provides(self):
+        return [i.__identifier__ for i in providedBy(self.context).flattened()]
