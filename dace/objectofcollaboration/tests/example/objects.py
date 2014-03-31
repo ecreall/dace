b500634db3885c0452f6186e0c0ecf6879ac0907
amen souissi <amensouissi@ecreall.com>
2014-03-31 11:10:58 +0200

    tests for allsubobject*() and subobject*() + corrections

---
+++ dace/objectofcollaboration/tests/example/objects.py
@@ -0,0 +1,63 @@
+from zope.interface import implements
+
+from dace.interfaces import IObject
+from dace.objectofcollaboration.object import (
+                COMPOSITE_UNIQUE, 
+                SHARED_UNIQUE,
+                COMPOSITE_MULTIPLE,
+                SHARED_MULTIPLE,
+                Object)
+
+
+class Object1(Object):
+    properties_def = {'composition_u':(COMPOSITE_UNIQUE, 'schared2_u', False),
+                      'composition_m':(COMPOSITE_MULTIPLE, 'schared21_u', False),
+                      'schared_u':(SHARED_UNIQUE, 'schared22_u', False),
+                      'schared_m':(SHARED_MULTIPLE, 'schared23_u', False)}
+
+    def __init__(self, **kwargs):
+        Object.__init__(self, **kwargs)
+
+
+class Object2(Object):
+    properties_def = {'schared2_u':(SHARED_UNIQUE, 'composition_u', False),
+                      'schared21_u':(SHARED_UNIQUE, 'composition_m', False),
+                      'schared22_u':(SHARED_UNIQUE, 'schared_u', False),
+                      'schared23_u':(SHARED_UNIQUE, 'schared_m', False)}
+
+    def __init__(self, **kwargs):
+        Object.__init__(self, **kwargs)
+
+
+class IObjectA(IObject):
+    pass
+
+
+class IObjectB(IObject):
+    pass
+
+
+class IObjectC(IObjectB):
+    pass
+
+
+class ObjectA(Object):
+    implements(IObjectA)
+    properties_def = {'composition_m':(COMPOSITE_MULTIPLE, None, False)}
+
+    def __init__(self, **kwargs):
+        Object.__init__(self, **kwargs)
+
+
+class ObjectB(Object):
+    implements(IObjectB)
+
+    def __init__(self, **kwargs):
+        Object.__init__(self, **kwargs)
+
+
+class ObjectC(ObjectB):
+    implements(IObjectC)
+
+    def __init__(self, **kwargs):
+        ObjectB.__init__(self, **kwargs)
---
+++ dace/objectofcollaboration/tests/test_getsubobject.py
@@ -0,0 +1,62 @@
+from dace.testing import FunctionalTests
+from dace.util import (
+    allSubobjectsOfType,
+    allSubobjectsOfKind,
+    subobjectsOfType,
+    subobjectsOfKind)
+from .example.objects import ObjectA, ObjectB, ObjectC, IObjectB, IObjectC
+
+
+class TestProperties(FunctionalTests):
+
+    def _create_objects(self):
+        object1 = ObjectA()
+        object2 = ObjectA()
+        object3 = ObjectB()
+        object4 = ObjectB()
+        object5 = ObjectC()
+     
+        object2.setproperty('composition_m', [object5])
+        object1.setproperty('composition_m', [object3, object4, object2])
+
+        self.app['object1'] = object1
+
+        return object1, object2, object3, object4, object5
+
+    def test_allSubobjects(self):
+        object1, object2, object3, object4, object5 = self._create_objects()
+        result = allSubobjectsOfType(object1, IObjectB)
+
+        self.assertEqual(len(result), 2)
+        self.assertTrue((object3 in result))
+        self.assertTrue((object4 in result))
+
+        result = allSubobjectsOfKind(object1, IObjectB)
+
+        self.assertEqual(len(result), 3)
+        self.assertTrue((object3 in result))
+        self.assertTrue((object4 in result))
+        self.assertTrue((object5 in result))
+
+        result = allSubobjectsOfType(object1, IObjectC)
+
+        self.assertEqual(len(result), 1)
+        self.assertTrue((object5 in result))
+
+    def test_subobjects(self):
+        object1, object2, object3, object4, object5 = self._create_objects()
+        result = subobjectsOfType(object1, IObjectB)
+
+        self.assertEqual(len(result), 2)
+        self.assertTrue((object3 in result))
+        self.assertTrue((object4 in result))
+
+        result = subobjectsOfKind(object1, IObjectB)
+
+        self.assertEqual(len(result), 2)
+        self.assertTrue((object3 in result))
+        self.assertTrue((object4 in result))
+
+        result = subobjectsOfType(object1, IObjectC)
+
+        self.assertEqual(len(result), 0)
