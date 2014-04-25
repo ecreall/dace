379743c1ebe4833c427fbcd6d0e0d73520366fdf
amen souissi <amensouissi@ecreall.com>
2014-04-25 16:31:48 +0200

    corrections

--- dace/processdefinition/processdef.py
+++ dace/processdefinition/processdef.py
@@ -213,6 +213,7 @@ class ProcessDefinition(Entity):
         query = object_provides_index.any((IProcess.__identifier__,)) & processid_index.eq(self.id)
         results = query.execute().all()
         processes = [p for p in results]
+        processes.sort()
         return processes        
 
     @property
