f6776489d2f3ebfaca4ab90e1caf32fe9c989a68
amen souissi <amensouissi@ecreall.com>
2014-07-23 21:28:13 +0200

    add machine

--- dace/objectofcollaboration/principal/__init__.py
+++ dace/objectofcollaboration/principal/__init__.py
@@ -1,6 +1,7 @@
 from substanced.principal import User as OriginUser
 from ..entity import Entity
 
+
 class User(OriginUser, Entity):
 
     def __init__(self, password=None, email=None, tzname=None, locale=None, **kwargs):
@@ -8,3 +9,7 @@ class User(OriginUser, Entity):
         Entity.__init__(self, kwargs)
 
 
+class Machine(User):
+
+    def __init__(self, password=None, email=None, tzname=None, locale=None, **kwargs):
+        super(Machine, self).__init__(password, email, tzname, locale, kwargs)
