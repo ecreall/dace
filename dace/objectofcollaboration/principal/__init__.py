99cb2c7ca93c7d60a03646dc7f024f92fb1db0ef
amen souissi <amensouissi@ecreall.com>
2014-10-22 14:16:43 +0200

    add reset password action

--- dace/objectofcollaboration/principal/__init__.py
+++ dace/objectofcollaboration/principal/__init__.py
@@ -1,4 +1,13 @@
+from pyramid_mailer import get_mailer
+from pyramid_mailer.message import Message
+from pyramid.renderers import render
+
 from substanced.principal import User as OriginUser
+from substanced.util import (
+    find_service,
+    acquire,
+    )
+
 from ..entity import Entity
 
 
@@ -7,6 +16,25 @@ class User(OriginUser, Entity):
     def __init__(self, password=None, email=None, tzname=None, locale=None, **kwargs):
         OriginUser.__init__(self, password, email, tzname, locale)
         Entity.__init__(self, **kwargs)
+
+    def email_password_reset(self, request):
+        """ Sends a password reset email."""
+        root = request.virtual_root
+        sitename = acquire(root, 'title', None) or 'Dace'
+        principals = find_service(self, 'principals')
+        reset = principals.add_reset(self)
+        # XXX should this really point at an SDI URL?
+        reseturl = request.resource_url(reset)
+        if not self.email:
+            raise ValueError('User does not possess a valid email address.')
+        message = Message(
+            subject = 'Account information for %s' % sitename,
+            recipients = [self.email],
+            body = render('dace:objectofcollaboration/principal/templates/resetpassword_email.pt',
+                          dict(reseturl=reseturl))
+            )
+        mailer = get_mailer(request)
+        mailer.send(message)
 
 
 class Machine(User):
