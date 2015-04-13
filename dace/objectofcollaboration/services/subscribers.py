38559bb316ce90932678a51633ef7b801cc05d17
amen souissi <amensouissi@ecreall.com>
2015-04-13 10:16:33 +0200

    remove deprecated process definitions

--- dace/objectofcollaboration/services/subscribers.py
+++ dace/objectofcollaboration/services/subscribers.py
@@ -5,6 +5,7 @@
 # licence: AGPL
 # author: Amen Souissi
 
+from zope.processlifetime import IDatabaseOpenedWithRoot
 from pyramid.events import subscriber
 from pyramid.threadlocal import get_current_request
 
@@ -20,3 +21,16 @@ def add_process_definition_container(event):
     request = get_current_request()
     request.root = root
     create_process_definition_container(root)
+
+
+
+@subscriber(IDatabaseOpenedWithRoot)
+def remove_definitions(event):
+    db = event.database
+    root = db.open().root()['app_root']
+    def_container = root['process_definition_container']
+    for definition in list(def_container.definitions):
+        if hasattr(definition, '_broken_object'):
+        	name = definition.__name__
+        	def_container.remove(name, send_events=False)
+        	def_container._definitions_value.remove(name)
