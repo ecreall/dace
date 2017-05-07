a16f350e864ce74d589387d16ad9694768c8d749
Vincent Fretin <vincent.fretin@gmail.com>
2017-05-07 19:56:50 +0200

    patch ObjectMap._find_resource with an optimized version

--- dace/patches.py
+++ dace/patches.py
@@ -6,3 +6,21 @@
 
 import tornado.ioloop
 tornado.ioloop._POLL_TIMEOUT = 0.2
+
+# The original ObjectMap._find_resource used pyramid.traversal.find_resource which uses pyramid.traversal.traverse which create a Request object...
+# We use a simpler implementation for performance.
+from substanced.objectmap import ObjectMap
+
+def _find_resource(self, context, path_tuple):
+    if context is None:
+        context = self.root
+
+    obj = context
+    # first segment is empty string
+    for segment in path_tuple[1:]:
+        # bypass substanced folder __getitem_ and get it directly from the BTree
+        obj = obj.data[segment]
+
+    return obj
+
+ObjectMap._find_resource = _find_resource
